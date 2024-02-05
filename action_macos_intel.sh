cbf7b41ef0fd5ec18a6fcb4ef2e9dbdcb3f9b1d8
aokihu <aokihu@gmail.com>
2024-02-05 16:40:53 +0800

    Add C++ build workflow and scripts for Linux and macOS

---
+++ action_macos_intel.sh
@@ -0,0 +1,17 @@
+# Prepare for linux platform
+
+# checkout taglib
+git submodule update --init --recursive
+
+# compile taglib
+cd taglib
+cmake -DCMAKE_BUILD_TYPE=MinSizeRel \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DWITH_ZLIB=OFF \
+    -DCMAKE_OSX_ARCHITECTURES="x86_64" \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=12.0 .
+make
+
+# compile tagtune
+cd ..
+make
--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <taglib/fileref.h>
 #include <taglib/tag.h>
 #include <iostream>
+#include <sstream>
+
+std::string toJsonString(const std::string& title, const std::string& artist, 
+                         const std::string& album, unsigned year, 
+                         const std::string& genre, int duration, 
+                         int channels, int bitrate) {
+    std::ostringstream jsonStream;
+    jsonStream << "{\n"
+               << "  \"title\": \"" << title << "\",\n"
+               << "  \"artist\": \"" << artist << "\",\n"
+               << "  \"album\": \"" << album << "\",\n"
+               << "  \"year\": " << year << ",\n"
+               << "  \"genre\": \"" << genre << "\",\n"
+               << "  \"duration\": " << duration << ",\n"
+               << "  \"channels\": " << channels << ",\n"
+               << "  \"bitrate\": " << bitrate << "\n"
+               << "}";
+    return jsonStream.str();
+}
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -10,14 +29,22 @@ int main(int argc, char *argv[]) {
 
     TagLib::FileRef f(argv[1]);
 
-    if (!f.isNull() && f.audioProperties()) {
+    if (!f.isNull()) {
+        TagLib::Tag *tag = f.tag();
         TagLib::AudioProperties *properties = f.audioProperties();
 
-        int length = properties->length(); // Duration in seconds
-        int channels = properties->channels(); // Number of channels
+        std::string json = toJsonString(
+            tag->title().toCString(true), 
+            tag->artist().toCString(true), 
+            tag->album().toCString(true), 
+            tag->year(), 
+            tag->genre().toCString(true), 
+            properties->length(), 
+            properties->channels(), 
+            properties->bitrate()
+        );
 
-        std::cout << "Duration: " << length << " seconds\n";
-        std::cout << "Channels: " << channels << std::endl;
+        std::cout << json << std::endl;
     } else {
         std::cerr << "Error reading file." << std::endl;
     }
