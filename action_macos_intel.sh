ba3b467588c5521bd3d829082583d966a7ea9245
aokihu <aokihu@gmail.com>
2024-02-05 16:58:15 +0800

    Update build configuration for Tagtune

--- action_macos_intel.sh
+++ action_macos_intel.sh
@@ -5,13 +5,15 @@ git submodule update --init --recursive
 
 # compile taglib
 cd taglib
+pwd
 cmake -DCMAKE_BUILD_TYPE=MinSizeRel \
     -DBUILD_SHARED_LIBS=OFF \
     -DWITH_ZLIB=OFF \
-    -DCMAKE_OSX_ARCHITECTURES="x86_64" \
+    -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
     -DCMAKE_OSX_DEPLOYMENT_TARGET=12.0 .
-make
+make -j0
 
 # compile tagtune
 cd ..
+pwd
 make
